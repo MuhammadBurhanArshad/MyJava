3eb2af1d2c02a58d46bf7e6fe4aa95cb8a7a5d74
MuhammadBurhanArshad <burhantheschoolboy@gmail.com>
2024-12-23 12:29:11 +0500

    Output

---
+++ _2_Output/Output.java
@@ -0,0 +1,25 @@
+package _2_Output;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("It is example of basic output");
+    
+        //Basic Pattern
+        /*
+        *
+        * *
+        * * *
+        * * * *
+        */
+    
+        System.out.println("\nPattern with println");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        
+        
+        System.out.println("\nPattern with print");
+        System.out.print("*\n* * \n");
+    }   
+}
